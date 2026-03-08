40f09b9aea2ba0eb9924b8cea54279611e80c29f
Eiphel <ch.hasanattasawar@gmail.com>
2026-03-09 00:51:37 +0500

    2nd

--- File Handling/File Handling/File Handling.cpp
+++ File Handling/File Handling/File Handling.cpp
@@ -1,20 +1,48 @@
-// File Handling.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include<iostream>
+#include<fstream>
+using namespace std;
 
-#include <iostream>
-
+class Employee
+{
+    int id,salary;
+    string name;
+	static ofstream records;
+public:
+	static void openfile() 
+	{
+		records.open("records.txt");
+	}
+	void input()
+	{
+		cout << "\n Enter Your Name, ID and Salary";
+		cin >> name >> id >> salary;
+	}
+	void output()
+	{
+		if (records.is_open())
+		{
+			records << "\n Name : " << name << "\t ID : " << id << "\t Salary : " << salary << endl;
+		}
+	}
+	static void closefile()
+	{
+		cout << "\n Records Stored in the File.";
+		records.close();
+	}
+	~Employee(){}
+};
 int main()
 {
-    std::cout << "Hello World!\n";
+	Employee e[3];
+	Employee::openfile();
+	for (int i = 0; i < 3; i++)
+	{
+		e[i].input();
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		e[i].output();
+	}
+	Employee::closefile();
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
