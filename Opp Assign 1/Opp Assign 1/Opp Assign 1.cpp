11dc940755c355ed2927a066b3444a3b55c3978f
Eiphel <ch.hasanattasawar@gmail.com>
2026-03-10 00:04:14 +0500

    int main ty shi

--- Opp Assign 1/Opp Assign 1/Opp Assign 1.cpp
+++ Opp Assign 1/Opp Assign 1/Opp Assign 1.cpp
@@ -12,9 +12,9 @@ public:
 	{ }
 	void create_acc()
 	{
-		cout << "\n Enter Your Username :\t";
+		cout << "\n== Enter Your Username :\t";
 		cin >> name;
-		cout << "\n Enter Your ID :\t";
+		cout << "\n== Enter Your ID :\t";
 		cin >> id;
 	}
 	~character(){}
@@ -37,7 +37,7 @@ public:
 		char choice;
 		cout << "\n================= Pick a Starting Weapon ==============";
 		cout << "\n======== SABER ======= Katana ========= Great Sword ===";
-		cout << "\n========   A   =======   B    ===========   C   =======";
+		cout << "\n========   A   =======   B    ===========   C   ======="<<endl;
 		cin >> choice;
 		if (choice == 'A' || choice == 'a')
 		{
@@ -65,6 +65,16 @@ public:
 			cout << "\n Invalid Input ! ";
 		}
 	}
+	void disp()
+	{
+		cout << "\n=========================  Character Details  =========================";
+		cout << "\n== Name : " << name << " --- " << id;
+		cout << "\n== Class : Vagabond";
+		cout << "\n== Weapon : " << weapon;
+		cout << "\n== Damage : " << damage;
+		cout << "\n== Stamina and Focus on Using Weapon : " << stamina << "\t" << focus;
+		cout << "\n== Posture : " << posture;
+	}
   ~Vagabond(){}
 };
 class Scholar : public character
@@ -84,7 +94,7 @@ public:
 		char choice;
 		cout << "\n=========================  Pick an Incantaion  =========================";
 		cout << "\n======== Comet Azure ======= Destined Death ========= Flame Finnesse ===";
-		cout << "\n============    A    ===========     B      =========     C   ==========";
+		cout << "\n============    A    ===========     B      =========     C   =========="<<endl;
 		cin >> choice;
 		if (choice == 'A' || choice == 'a')
 		{
@@ -115,6 +125,17 @@ public:
 			cout << "\n Invalid Input ! ";
 		}
 	}
+	void disp()
+	{
+		cout << "\n=========================  Character Details  =========================";
+		cout << "\n== Name : " << name << " --- " << id;
+		cout << "\n== Class : Scholar";
+		cout << "\n== Weapon : Staff With Spell : " << spell;
+		cout << "\n== Damage : " << damage;
+		cout << "\n== Spell Affinity : " << affinity;
+		cout << "\n== Stamina and Focus on Using Weapon : " << stamina << "\t" << focus;
+		cout << "\n== Posture : " << posture;
+	}
 	~Scholar(){}
 };
 class Ironeye : public character
@@ -132,12 +153,12 @@ public:
 		stamina = 250;
 		focus = 50;
 	}
-	void choose_Bow()
+	void choose_bow()
 	{
 		char choice;
 		cout << "\n=========================  Pick a Bow  =========================";
 		cout << "\n======== Dark Bow ======= Cross-Bow ========= Golem Bow ========";
-		cout << "\n========     A    =======     B     =========      C   =========";
+		cout << "\n========     A    =======     B     =========      C   =========" << endl;
 		cin >> choice;
 		if (choice == 'A' || choice == 'a')
 		{
@@ -173,5 +194,52 @@ public:
 			cout << "\n Invalid Input ! ";
 		}
 	}
+	void disp()
+	{
+		cout << "\n=========================  Character Details  =========================";
+		cout << "\n== Name : " << name<<" --- "<<id;
+		cout << "\n== Class : Ironeye";
+		cout << "\n== Weapon : "<<bow;
+		cout << "\n== Damage : " << damage;
+		cout << "\n== Range : " << range;
+		cout << "\n== Fire_rate : " << fire_rate;
+		cout << "\n== Stamina and Focus on Using Weapon : " << stamina << "\t" << focus;
+		cout << "\n== Posture : " << posture;
+	}
 	~Ironeye() {}
 };
+
+int main()
+{
+	char pick;
+	character c;
+	cout << "\n=========================  Character Creation  =========================";
+	cout << "\n=========================     Pick a Class     =========================";
+	c.create_acc();
+	cout << "\n========== Vagabond ============= Scholar ============= Ironeye ========";
+	cout << "\n==========     A    =============    B    =============    C    ========"<<endl;
+	cin >> pick;
+	if (pick == 'A' || pick == 'a')
+	{
+		Vagabond*a = new Vagabond(); 
+		a->choose_weapon();
+		a->disp();
+	}
+	else if (pick == 'B' || pick == 'b')
+	{
+		Scholar*a = new Scholar();
+		a->choose_spells();
+		a->disp();
+	}
+	else if (pick == 'C' || pick == 'c')
+	{
+		Ironeye*a = new Ironeye();
+		a->choose_bow();
+		a->disp();
+	}
+	else
+	{
+		cout << "\n Invalid Input !";
+	}
+	return 0;
+}
